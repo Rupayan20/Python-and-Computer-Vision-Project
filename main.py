375c0ff80ce8e20f31ca4ad4f44b0f96592ad9fe
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-01-08 22:57:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+
+fashion_mnist= keras.datasets.fashion_mnist
+(train_images,train_labels),(test_images,test_labels)= fashion_mnist.load_data()
+train_images= train_images/255.0
+train_labels= train_images/255.0
+train_images[0].shape
+
+train_images= train_images.reshape(len(train_images),28,28,1)
+test_images= test_images.reshape(len(test_images),28,28,1)
