8d8295b7203a9f354612ec00f546907ec7cf9696
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-01-09 22:32:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,25 @@ train_images[0].shape
 
 train_images= train_images.reshape(len(train_images),28,28,1)
 test_images= test_images.reshape(len(test_images),28,28,1)
+
+
+def build_model(hp):
+  model= keras.Sequential([
+      keras.layers.Conv2D(
+          filters= hp.Int('conv_1_filter', min_value=32, max_value=128, step=16),
+          kernel_size= hp.Choice('conv_1_kernel', values=[3,5]),
+          activation='relu',
+          input_shape= (28,28,1)
+      ),
+      keras.layers.Conv2D(
+          filters= hp.Int('conv_2_filter', min_value=32, max_value=128, step=16),
+          kernel_size= hp.Choice('conv_2_kernel', values=[3,5]),
+          activation='relu'
+      ),
+      keras.layers.Flatten(),
+      keras.layers.Dense(
+        units= hp.Int('dense_1_units', min_value=32, max_value=128, step=16),
+        activation='relu'
+      ),
+      keras.layers.Dense(10, activation='softmax')  #output layer
+  ])
